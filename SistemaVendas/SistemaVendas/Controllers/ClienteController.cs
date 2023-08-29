dfd323dfe140d057f554c2bbac1d895ad86cee24
Jonas <jonas.jsilva@e-ai.com.br>
2023-08-29 16:04:56 -0300

    Publicando no git

---
+++ SistemaVendas/SistemaVendas/Controllers/ClienteController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SistemaVendas.Models;
+
+namespace SistemaVendas.Controllers
+{
+    public class ClienteController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            ViewBag.ListaClientes = new ClienteModel().ListarTodosClientes();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Cadastro(int? id)
+        {
+            if (id != null) 
+            {
+                //Carregar o registro do cliente em uma viewBag
+                ViewBag.Cliente = new ClienteModel().RetornarCliente(id);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(ClienteModel cliente)
+        {
+            if (ModelState.IsValid) 
+            {
+                cliente.Gravar();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Editar(int? id)
+        {
+            if (id != null)
+            {
+                //Carregar o registro do cliente em uma viewBag
+                ViewBag.Cliente = new ClienteModel().RetornarCliente(id);
+            }
+            return View();
+        }
+
+        public IActionResult Excluir(int id)
+        {
+            ViewData["IdExcluir"] = id;
+            ViewBag.IdExcluir = id;
+            var cliente = new ClienteModel().RetornarCliente(id);
+
+            return View(cliente);
+        }
+
+        public IActionResult ExcluirCliente(int id)
+        {
+            new ClienteModel().Excluir(id);
+            return View();
+        }
+    }
+}
---
+++ SistemaVendas/SistemaVendas/Models/ClienteModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SistemaVendas.Uteis;
+using System.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaVendas.Models
+{
+    public class ClienteModel
+    {
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Informe o Nome do Cliente")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe o Cpf ou Cnpj do Cliente")]
+        public string CPF { get; set; }
+
+        [Required(ErrorMessage = "Informe o Email do Cliente")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage ="O email informado é invalido!")]
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+
+        public List<ClienteModel> ListarTodosClientes() 
+        {
+            List<ClienteModel> lista = new List<ClienteModel>();
+            ClienteModel item;
+            DAL objDAL = new DAL();
+            string sql = "select id, nome, cpf_cnpj, email, senha from Cliente order by nome asc";
+            DataTable dt = objDAL.RetDataTable(sql);
+
+            for (int i = 0; i < dt.Rows.Count; i++) 
+            {
+                item = new ClienteModel
+                {
+                    Id = dt.Rows[i]["Id"].ToString(),
+                    Nome = dt.Rows[i]["Nome"].ToString(),
+                    CPF = dt.Rows[i]["cpf_cnpj"].ToString(),
+                    Email = dt.Rows[i]["Email"].ToString(),
+                    Senha = dt.Rows[i]["Senha"].ToString(),
+                };
+                lista.Add(item);
+            }
+            return lista;
+        }
+
+        public ClienteModel RetornarCliente(int? id)
+        {
+            ClienteModel item;
+            DAL objDAL = new DAL();
+            string sql = $"select id, nome, cpf_cnpj, email, senha from Cliente where id = '{id}' order by nome asc";
+            DataTable dt = objDAL.RetDataTable(sql);
+
+            
+            
+          item = new ClienteModel
+          {
+              Id = dt.Rows[0]["Id"].ToString(),
+              Nome = dt.Rows[0]["Nome"].ToString(),
+              CPF = dt.Rows[0]["cpf_cnpj"].ToString(),
+              Email = dt.Rows[0]["Email"].ToString(),
+              Senha = dt.Rows[0]["Senha"].ToString(),
+          };
+               
+            
+          return item;
+        }
+
+        //Metodo de insert ou update 
+        public void Gravar() 
+        {
+            DAL objDAL = new DAL();
+            string sql = string.Empty;
+
+            if (Id != null) 
+            {
+                 sql = $"update cliente set nome='{Nome}', cpf_cnpj='{CPF}', email='{Email}' where id='{Id}'";
+            }
+            else 
+            {
+                 sql = $"insert into cliente (nome, cpf_cnpj, email, senha) values ('{Nome}', '{CPF}', '{Email}', '123456')";
+            }
+            
+            objDAL.ExecutarComandoSQL(sql);
+        }
+        
+        //Metodo Excluir
+        public void Excluir(int id) 
+        {
+            DAL objDAL = new DAL();           
+
+            string sql = $"delete from cliente where id='{id}'";
+            objDAL.ExecutarComandoSQL(sql);
+        }
+    }
+}
